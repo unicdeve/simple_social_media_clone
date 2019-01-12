83ada83c7c00e401c52dc5c47084ea85e9189cdf
Taiwo Ogunola <uniqueomokenny@gmail.com>
2019-01-12 22:24:01 +0100

    connected the group urls

--- simplesocial/groups/views.py
+++ simplesocial/groups/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.contrib.auth.mixins import (LoginRequiredMixin, PermissionRequiredMixin)
 from django.core.urlresolvers import reverse
 from django.views import generic
 from .models import Group, GroupMember
+
+# Create your views here.
 
 class CreateGroup(LoginRequiredMixin, generic.CreateView):
     fields = ('naame', 'description')
